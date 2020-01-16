fd41e42eb2448be7f33b6cadce583c05bfa50947
Jackie <JGtheophila19@gmail.com>
2020-01-16 13:07:13 -0500

    debugging

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -1,17 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-/*using statement*/
 
-namespace HelloWorld
+namespace CSharpFundamentals
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var numbers = new List<int>{1, 2, 3, 4, 5, 6};
+            var smallests = GetSmallest(numbers, 3);
+            
+            foreach(var number in smallests)
+                Console.WriteLine(number);
         }
+        
+        public static List<int> GetSmallest(List<int> list, int count)
+        {
+            var smallest = new List<int>();
+            
+            while(smallest.Count< count)
+            {
+                var min = GetSmallest(list);
+                smallest.Add(min);
+                list.Remove(min);
+            }
+            return smallest;
+        }
+        
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest 
+            var min = list[0];
+            
+            for(var i = 1; i < list.Count; i++)
+            {
+               if(list[i] > min)
+                   min = list[i]; 
+            }
+            return min;
+        
+        
     }
 }
